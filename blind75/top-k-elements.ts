0b7a9c0d147af9453aca01bdc9aa6432311ba38c
rknm-cell <racknahm@gmail.com>
2025-07-26 14:57:18 -0400

    top k elements

---
+++ blind75/top-k-elements.ts
@@ -0,0 +1,20 @@
+topKFrequent(nums, k) {
+        const count = {}
+        const freq = Array.from({ length: nums.length + 1}, () => []);
+
+        for (const n of nums){
+            count[n] = (count[n] || 0) + 1
+        }
+        for (const n in count){
+            freq[count[n]].push(parseInt(n));
+        }
+        const res = [];
+        for (let i = freq.length - 1; i > 0; i--){
+            for (const n of freq[i]){
+                res.push(n);
+                if (res.length === k){
+                    return res;
+                }
+            }
+        }
+    }
